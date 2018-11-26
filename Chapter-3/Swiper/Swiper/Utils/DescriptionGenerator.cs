f741ff5cb83c6b0d3c17cac09fda055e609c704f
Johan Karlsson <johan.kson@gmail.com>
2018-11-26 20:58:30 +0100

    Added code for chapter 3

---
+++ Chapter-3/Swiper/Swiper/Utils/DescriptionGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Swiper.Utils
+{
+    public class DescriptionGenerator
+    {
+        private string[] _adjectives = { "nice", "horrible", "great", "terribly old", "brand new" };
+        private string[] _other = { "picture of grandpa", "car", "photo of a forest", "duck" };
+        private static Random random = new Random();
+
+        public string Generate()
+        {
+            var a = _adjectives[random.Next(_adjectives.Count())];
+            var b = _other[random.Next(_other.Count())];
+            return $"A {a} {b}";
+        }
+    }
+}
---
+++ Chapter-3/Swiper/Swiper/Utils/Picture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swiper.Utils
+{
+    public class Picture
+    {
+        public Uri Uri { get; set; }
+        public string Description { get; set; }
+
+        public Picture()
+        {
+            Uri = new Uri($"https://picsum.photos/400/400/?random&ts={DateTime.Now.Ticks}");
+
+            var generator = new DescriptionGenerator();
+            Description = generator.Generate();
+        }
+    }
+}
