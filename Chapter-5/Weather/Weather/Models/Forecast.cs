0e01e5c6c0f13f46f9473e1ca76cc3a980ab48cc
Daniel Hindrikes <daniel@hindrikes.se>
2018-12-19 10:51:21 +0100

    removed duplicated code

--- Chapter-5/Weather/Weather/Models/Forecast.cs
+++ Chapter-5/Weather/Weather/Models/Forecast.cs
@@ -9,14 +9,4 @@ namespace Weather.Models
         public string City { get; set; }
         public List<ForecastItem> Items { get; set; }
     }
-
-    public class ForecastItem
-    {
-        public DateTime DateTime { get; set; }
-        public string TimeAsString => DateTime.ToShortTimeString();
-        public double Temperature { get; set; }
-        public double WindSpeed { get; set; }
-        public string Description { get; set; }
-        public string Icon { get; set; }
-    }
 }
