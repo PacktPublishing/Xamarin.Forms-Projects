e1b2f279d1682db95b0f8fa1cefafe157b5c0d14
Daniel Hindrikes <daniel@hindrikes.se>
2019-10-23 23:05:59 +0200

    chapter 6 code

--- Chapter06/MeTracker/MeTracker/Views/MainView.xaml.cs
+++ Chapter06/MeTracker/MeTracker/Views/MainView.xaml.cs
@@ -23,7 +23,13 @@ namespace MeTracker.Views
             MainThread.BeginInvokeOnMainThread(async () =>
             {
                 var location = await Geolocation.GetLastKnownLocationAsync();
-               Map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(location.Latitude, location.Longitude), Distance.FromKilometers(5)));
+
+                if(location == null)
+                {
+                    location = await Geolocation.GetLocationAsync();
+                }
+
+                Map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(location.Latitude, location.Longitude), Distance.FromKilometers(5)));
             });
         }
 	}
